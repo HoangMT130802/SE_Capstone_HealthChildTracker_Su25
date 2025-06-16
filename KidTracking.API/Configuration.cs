0e6ed37714c1c07406652e6410c92bb6ce251790
Nguyen Thanh Dung <dungntse170523@fpt.edu.vn>
2025-06-16 21:36:13 +0700

    Update code base

---
+++ KidTracking.API/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using Repositories.Common;
+using Repositories.Interfaces;
+using Services.Common;
+
+namespace KidTracking.API
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddAPIConfiguration(this IServiceCollection services)
+        {
+            // Common
+            services.AddHttpContextAccessor();
+            services.AddAutoMapper(typeof(MapperProfile).Assembly);
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
