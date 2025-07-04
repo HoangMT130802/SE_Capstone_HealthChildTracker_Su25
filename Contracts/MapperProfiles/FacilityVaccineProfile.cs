bc91fce485d3ee902457b4426c29081d650b3378
Nguyen Thanh Dung <dungntse170523@fpt.edu.vn>
2025-07-04 16:04:30 +0700

    fix FacilityVaccine

--- Contracts/MapperProfiles/FacilityVaccineProfile.cs
+++ Contracts/MapperProfiles/FacilityVaccineProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Contracts.DTOs.FacilityVaccine;
+using Contracts.DTOs.Vaccine;
 using Repositories.Entities;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,6 @@ namespace Contracts.MapperProfiles
     {
         public FacilityVaccineProfile()
         {
-            // Ánh xạ từ CreateFacilityVaccineDTO sang FacilityVaccine
             CreateMap<CreateFacilityVaccineDTO, FacilityVaccine>()
                 .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
                 .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
@@ -21,16 +21,20 @@ namespace Contracts.MapperProfiles
                 .ForMember(dest => dest.Facility, opt => opt.Ignore())
                 .ForMember(dest => dest.Vaccine, opt => opt.Ignore());
 
-        // Ánh xạ từ UpdateFacilityVaccineDTO sang FacilityVaccine
-        CreateMap<UpdateFacilityVaccineDTO, FacilityVaccine>()
+            // Ánh xạ từ UpdateFacilityVaccineDTO sang FacilityVaccine
+            CreateMap<UpdateFacilityVaccineDTO, FacilityVaccine>()
                 .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
                 .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
                 .ForMember(dest => dest.FacilityVaccineId, opt => opt.Ignore())
                 .ForMember(dest => dest.Facility, opt => opt.Ignore())
                 .ForMember(dest => dest.Vaccine, opt => opt.Ignore());
 
-        // Ánh xạ từ FacilityVaccine sang FacilityVaccineDTO
-        CreateMap<FacilityVaccine, FacilityVaccineDTO>();
+            // Ánh xạ từ FacilityVaccine sang FacilityVaccineDTO
+            CreateMap<FacilityVaccine, FacilityVaccineDTO>()
+                .ForMember(dest => dest.Vaccine, opt => opt.MapFrom(src => src.Vaccine));
+
+            // Ánh xạ từ Vaccine sang VaccineDTO
+            CreateMap<Vaccine, VaccineDTO>();
         }
 }
 }
